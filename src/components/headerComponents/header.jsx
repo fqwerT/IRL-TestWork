baec8efca113602ff51e3c446ddada213937977e
fqwerT <108628673+fqwerT@users.noreply.github.com>
2022-10-18 15:10:13 +0400

    Update header.jsx

--- src/components/headerComponents/header.jsx
+++ src/components/headerComponents/header.jsx
@@ -8,9 +8,9 @@ export const Header = () => {
   return (
     <div className={style.header}>
       <div className={style.header__box}>
-      <Link className={style.header__item} to="/HomePage">Home</Link>
-      <Link className={style.header__item} to="/loginPage">Login</Link>
-      <Link className={style.header__item} to="/registerPage">Register</Link>
+      <Link className={style.header__item} to="HomePage">Home</Link>
+      <Link className={style.header__item} to="loginPage">Login</Link>
+      <Link className={style.header__item} to="registerPage">Register</Link>
       </div>
     </div>
   );
